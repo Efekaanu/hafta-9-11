c10e7d98991160510676ec790ddf83195ef33e05
Efekaanu <155834429+Efekaanu@users.noreply.github.com>
2024-02-19 21:29:19 +0300

    Döngüler

---
+++ döngüler.py
@@ -0,0 +1,23 @@
+#Sayılar=[2,3,6,7,8,89,43,76,34,23]
+
+
+#for sayı in Sayılar:
+ #   if(sayı==0)
+
+
+
+
+#sayı=int(input("Bir sayı giriniz"))
+#sayaç=0
+#while sayaç<=sayı:
+#print(sayaç)
+#sayaç=-1
+
+#liste[]
+#i=0
+#while i<5:
+#    sayı:int(input("Bir sayı giriniz"))
+ #   liste.append(sayı)
+#i=-1
+#print(liste)
+
