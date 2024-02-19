05088c61322c7d7a47a730f55578f1d87ab5873f
Efekaanu <155834429+Efekaanu@users.noreply.github.com>
2024-02-19 21:29:04 +0300

    Fonksiyonlar

---
+++ fonksiyonlar.py
@@ -0,0 +1,29 @@
+#def selamdünya():
+ #   print("hello world")
+
+#selamdünya()
+
+#def hoşgeldin(name):
+ #   print("hoşgeldin "+ name)
+#    name=input('adınızı giriniz')
+
+#hoşgeldin('Efe Kaan')
+
+#def fonksiyon(şehir = "karabük"):
+ #   print("en sevdiğin şehir"+ şehir)
+
+#fonksiyon("istanbul")
+#fonksiyon()
+
+#def sayı(x):
+ #   x=x+5
+  #  return x
+
+#sonuç=sayı(5)
+#print(sonuç)
+
+def fonksiyon(x,y):
+    toplam=5+y
+    return toplam
+sonuç=fonksiyon(30)
+print(sonuç)
